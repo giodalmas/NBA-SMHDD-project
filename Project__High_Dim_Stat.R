e8f7ae7c61d2208ab608765e2514064ff2d3f629
Wupadova <92895452+Wupadova@users.noreply.github.com>
2022-12-30 19:02:28 +0100

    Add files via upload

--- Project__High_Dim_Stat.R
+++ Project__High_Dim_Stat.R
@@ -3,7 +3,6 @@
 # The 2k player dataset
 data_2k <- read.csv('C:\\Users\\Acer\\Desktop\\2_1\\Statistica\\Project_NBA\\2K20_ratings.csv')
 
-
 # 2021 Player dataset
 data_player_game <- read.csv('C:\\Users\\Acer\\Desktop\\2_1\\Statistica\\Project_NBA\\nba2021_per_game.csv')
 data_player_adv <- read.csv('C:\\Users\\Acer\\Desktop\\2_1\\Statistica\\Project_NBA\\nba2021_advanced.csv')
@@ -20,6 +19,8 @@ data_team_playoff <- data_team_playoff[data_team_playoff$season == '2020-21', ]
 
 # Load the library for the select function
 library(dplyr)
+########################################################################################
+########################################################################################
 #=======================================================================================
 # Merge the player data
 data <- left_join(data_player_game, data_player_adv, by = "Player", suffix = c("", ".y")) %>%
@@ -29,35 +30,56 @@ data <- left_join(data, data_2k, by = "Player", suffix = c("", ".y")) %>%
   select(-ends_with(".y"))
 
 #=======================================================================================
-# Merge the player and team data
+# Merge the player and team dataset by the team names
 #---------------------------------------------------------------------------------------
-# Change the name of the teams to their abbreviations
-# Extract all the team names of team dataset
-
-
-team_name <- data_team['TEAM']
-
-# Extract the unique team name abbreviations of data
-
-# Change Tm of data to TEAM
+# Change the name of the teams to their abbreviations(on data_team dataset)
+# Change name of the column 'Tm' of data to 'TEAM'
 colnames(data)[4] <- 'TEAM'
 
+# Order the data_team dataset by team names
+data_team <- data_team %>% arrange(TEAM)
+
+# Extract all the team names of both dataset
+team_name <- data_team['TEAM']
 name_abb <- unique(data$TEAM)
-name_abb
-
-
-
-# Order the names by alphabetical order
+length(name_abb)
 #---------------------------------------------------------------------------------------
+# Notice that the length of name_abb is 31 which means that it includes one more category
+# (TOT) which means the player plays for different teams in that season, we should remove
+# the players with TOT.
+#---------------------------------------------------------------------------------------
+data <- subset(data, TEAM != 'TOT')
 
-####################################################################################
-####################################################################################
-# change the team names and merge team data with player data
+# Sort the team names of the general dataset (data dataset)
+name_abb <- unique(data$TEAM)   # update the name_abb
+name_abb <- sort(name_abb)
 
+#---------------------------------------------------------------------------------------
+# Notice that the listed names have the correct order of the full and their abbreviations 
+# except the Charlotte Hornets and Chicago Bulls, thus we have to swap them (team_name).
+#---------------------------------------------------------------------------------------
+cho <- data_team[4, ]    # 4th row of data_team is Charlotte, should be Chicago 
+data_team[4, ] <- data_team[5, ]
+data_team[5, ] <- cho
 
+data_team  # check again the names
 
+# Assign the names of teams of team dataset into its abbreviations
+data_team$TEAM <- name_abb
+data_team$TEAM
 
-data <- left_join(data, data_team, by = "Player")
+#=======================================================================================
+# MERGE TEAM DATASET(DATA_TEAM) WITH PLAYER DATASET(DATA)
+#---------------------------------------------------------------------------------------
+# remove the column of teamstatspk of data_team
+data_team <- select(data_team, -c(teamstatspk, SEASON))
+data_team
+#---------------------------------------------------------------------------------------
+# Merge two dataset(data and data_team)
+data <- left_join(data, data_team, by = "TEAM")
+
+########################################################################################
+########################################################################################
 #=======================================================================================
 # Remove the redundant columns
 data <- data[,!duplicated(names(data))]
@@ -94,3 +116,4 @@ View(data)
 
 
 rm(list = ls())
+
